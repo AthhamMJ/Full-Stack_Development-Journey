484badedfc234a71c01dcf2778c319cd557cf1a3
AthhamMJ <athham.uki@dreamspace.acedamy>
2026-06-18 23:58:20 +0530

    Attached the files to 1-20

---
+++ Back_End/Assignments/Iterations/Pattern Basics/21.js
@@ -0,0 +1,8 @@
+function star_up(n){
+    star = "*"
+    for(n > 0; n--;){
+        console.log(star);
+        star+="*"
+    }
+}
+star_up(5)
---
+++ Back_End/Assignments/Iterations/Pattern Basics/22.js
@@ -0,0 +1,5 @@
+function star_down(n){
+    let star = "*"
+    console.log(String.star * n);
+}
+star_down(5)
