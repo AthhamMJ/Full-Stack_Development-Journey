131b78fb12b9290157366e5d8b719e79e1831687
AthhamMJ <athham.uki@dreamspace.acedamy>
2026-06-21 01:16:10 +0530

    Attahced the new files

--- Back_End/Assignments/Iterations/Pattern Basics/21.js
+++ Back_End/Assignments/Iterations/Pattern Basics/21.js
@@ -1,5 +1,5 @@
 function star_up(n){
-    star = "*"
+    let star = "*"
     for(n > 0; n--;){
         console.log(star);
         star+="*"
--- Back_End/Assignments/Iterations/Pattern Basics/22.js
+++ Back_End/Assignments/Iterations/Pattern Basics/22.js
@@ -1,5 +1,10 @@
 function star_down(n){
-    let star = "*"
-    console.log(String.star * n);
+    let star = "*".repeat(n);
+
+    for(; n > 0; n--){
+        console.log(star);
+        star = star.slice(0, -1);
+    }
 }
-star_down(5)
+
+star_down(5);
