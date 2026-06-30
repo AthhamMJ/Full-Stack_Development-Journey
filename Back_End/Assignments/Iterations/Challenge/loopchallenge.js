0403d28c243bfdfb20cd5260c81940f08839ea65
AthhamMJ <athham.uki@dreamspace.acedamy>
2026-06-30 23:53:00 +0530

    Attached the files

--- Back_End/Assignments/Iterations/Challenge/loopchallenge.js
+++ Back_End/Assignments/Iterations/Challenge/loopchallenge.js
@@ -5,19 +5,20 @@ function main(){
     factorial(5);
     reverse(23547);
     sumofdigit(12345);
-    let n = 100; 
-    for (let i = 1; i <= n; i++) {
-        if (palindrome(i)) {
-            console.log(i);
-        }
-    }
+    palindrome(100);
+    prime(100);
+    factor(28);
+    fibenocci(28);
+    large(1254536569);
 }
+
 function onetotwenty(n){
     for(let count = 1; count <= n; count++){
         console.log(count);
     }
     console.log("");
 }
+
 function sumfifty(n){
     let sum = 0;
     for(let count = 1; count <= n; count++){
@@ -26,12 +27,14 @@ function sumfifty(n){
     console.log(sum);
     console.log("");
 }
+
 function multable(n){
     for(let count = 1; count <= n; count++){
         console.log(count+ " X " +n+ " = " + (count * n));
     }
     console.log("");
 }
+
 function factorial(n){
     let fact = 1
     for(; n > 0; n--){
@@ -40,6 +43,7 @@ function factorial(n){
     console.log(fact)
     console.log("");
 }
+
 function reverse(n){
     let reverse = 0;
     //reversing the number
@@ -52,6 +56,7 @@ function reverse(n){
     console.log(reverse);
     console.log("");
 }
+
 function sumofdigit(n){
     let sum = 0;
     for(; n > 0; n = Math.floor(n / 10)){
@@ -60,15 +65,67 @@ function sumofdigit(n){
     console.log(sum);
     console.log("");
 }
+
+
 function palindrome(n) {
-    let number = n;
     let reverse = 0;
-    for (;n > 0; n = Math.floor(n / 10)) {
-        reverse = reverse * 10 + (n % 10);
+    for(let i = 1; i < n; i++){
+        let number = i;
+        let reverse = 0
+        for (; number > 0; number = Math.floor(number / 10)) {
+            reverse = reverse * 10 + (i % 10); 
+        }
+        if(reverse === i){
+            console.log(i);
+        }
     }
-    return reverse === number;
     console.log("");
 }
 
+function prime(n){
+    for(let count = 2; count <= 100; count++){
+        let prime = true;
+        for(let div = 2; div <= Math.sqrt(count); div++){
+            if(count % div === 0){
+                prime = false;
+                break;
+            }
+        }
+        if(prime){
+            console.log(count);
+        }
+    }
+    console.log("");
+}
 
+function factor(n){
+    for(let div = 1; div <= (n/2); div++){
+        if(n % div == 0){
+            console.log(div+" X "+ (n / div)+ " = "+ n);
+        }
+    }
+}
+
+function fibenocci(n){
+    for(let div = 1; div <= (n/2); div++){
+        if(n % div == 0){
+            console.log(div+" X "+ (n / div)+ " = "+ n);
+        }
+    }
+    console.log("")
+}
+
+function large(n){
+    numarray = [];
+    for(; n > 0; n = Math.floor(n / 10)){
+        numarray.push(n % 10)
+    }
+    let max = numarray[0]
+    for(let i = 1; i < numarray.length; i++){
+        if(max < numarray[i]){
+            max = numarray[i]
+        }
+    }
+    console.log(max);
+}
 main();
