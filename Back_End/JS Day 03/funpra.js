91059923095f917b299df7929da90e0d7c7fd43d
AthhamMJ <athham.uki@dreamspace.academy>
2026-07-11 21:35:06 +0530

    Delete Back_End/JS Day 03 directory

--- Back_End/JS Day 03/funpra.js
+++
@@ -1,8 +0,0 @@
-console.log("Running...");
-function calc(num1, num2){
-    let.n1 = Number(document.getElementById("num1"));
-    let.n2 = Number(document.getElementById("num2"));
-
-    let sum = n1+ n2;
-    console.log("The Answer is" +sum);
-}
