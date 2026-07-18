178dfbf89ba309d8847d5e86c4f00c0f021e8992
AthhamMJ <athham.uki@dreamspace.acedamy>
2026-07-18 19:20:53 +0530

    feat; folder management

---
+++ Back_End/Assignments/Arrays/Interns/1.js
@@ -0,0 +1,27 @@
+function DuplicateRemover() {
+    let numbers = [1, 2, 4, 5, 2, 5, 6, 7, 8, 9, 10, 9, 9];
+    let numbers2 = [];
+
+    for (let i = 0; i < numbers.length; i++) {
+
+        let found = false;
+
+        for (let j = 0; j < numbers2.length; j++) {
+
+            if (numbers[i] === numbers2[j]) {
+                found = true;
+                break;
+            }
+
+        }
+
+        if (!found) {
+            numbers2.push(numbers[i]);
+        }
+
+    }
+
+    console.log(numbers2);
+}
+
+DuplicateRemover();
